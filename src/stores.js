639dffc00d9ba45233f19e835e881e970eb9dd0e
Jhon Suarez <sistec_147@hotmail.com>
2020-06-03 20:20:33 +0000

    Readme

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
 let token_api = writable('');
-const url_api = " https://jhonsuarez.tunnels.tuordenas.com/";
+const url_api = "https://jhonsuarez.tunnels.tuordenas.com/";
 
 export { token_api, url_api };
